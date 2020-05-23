c02ee6b63bb162f3682d0932af234eb68277eaf2
Tin Trinh <trinh.tin@hamee.co.jp>
2020-05-23 15:58:27 +0900

    Initial Commit

---
+++ Porodomo Timer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Porodomo Timer
+//
+//  Created by trinh.tin on 2020/05/23.
+//  Copyright © 2020 New Tetra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
